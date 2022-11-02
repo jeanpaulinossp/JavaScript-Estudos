2fe802f0ed231a5a62a8cc0cb1600c003bba5db9
Jean Paulino <jean.ssparaiso@gmail.com>
2022-11-02 13:20:32 -0300

    att

--- Origamid/JS - Iniciante/Exemplos/003-Map/script.js
+++ Origamid/JS - Iniciante/Exemplos/003-Map/script.js
@@ -1,5 +1,10 @@
 const aulas = ["matematica", "portugues", "ciencia", "geografia", "computacao"];
 
 aulas.map((item, index, array) => {
+  console.log(item);
+  console.log(index);
+  console.log(array);
   console.log(array[index + 1]);
 });
+
+// o map retorna uma array com os resultados acumulados
---
+++ Origamid/JS - Iniciante/Exemplos/004.Reduce/script.js
@@ -0,0 +1,19 @@
+const aulas = [10, 25, 30];
+
+const retorno = aulas.reduce((acumulador, item, index, array) => {
+  return acumulador + item;
+}, 0 /*esse é o valor do acumulador inicial*/);
+
+console.log(retorno);
+
+// o 0 é do acumulador, depois no próximo retorno, ele pega o 0 e soma com o primeiro item, retornando 10, depois com o segundo item (10 + 25) e por ai vai
+// quando o acumulador não é passado o reduce usa o primeiro valor como acumulador
+// o reduce retorna somente o ultimo valor da iteração feita
+
+const numeros = [10, 25, 30, 4, 54, 33, 22];
+
+const maiorNumero = numeros.reduce((anterior, atual) => {
+  return anterior > atual ? anterior : atual;
+});
+
+console.log("O Maior numero é:", maiorNumero);
